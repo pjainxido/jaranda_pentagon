cebbfa6b3669f3ed516459d04a87ecf780607371
Jiyeol Jake Lee <jake.lee4006@gmail.com>
2021-08-02 23:21:01 +0900

    [CHORE] firebase.js : 변경된 env 변수 값 적용

--- src/firebase.js
+++ src/firebase.js
@@ -4,12 +4,12 @@ import firebase from 'firebase/app';
 import 'firebase/firestore';
 
 const firebaseConfig = {
-	apiKey: process.env.REACT_APP_API_KEY,
-	authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-	projectId: process.env.REACT_APP_PROJECT_ID,
-	storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-	messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-	appId: process.env.REACT_APP_APP_ID,
+	apiKey: process.env.REACT_APP_FB_API_KEY,
+	authDomain: process.env.REACT_APP_FB_AUTH_DOMAIN,
+	projectId: process.env.REACT_APP_FB_PROJECT_ID,
+	storageBucket: process.env.REACT_APP_FB_STORAGE_BUCKET,
+	messagingSenderId: process.env.REACT_APP_FB_MESSAGING_SENDER_ID,
+	appId: process.env.REACT_APP_FB_APP_ID,
 };
 
 firebase.initializeApp(firebaseConfig);
