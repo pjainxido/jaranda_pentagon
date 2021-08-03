2b6e8874e2a729f6d8b123b8f2665552ed57459b
JOHNKIM-KK <76652614+JOHNKIM-KK@users.noreply.github.com>
2021-08-03 19:13:40 +0900

    [FEAT]  사용자 계정 페이지 구현

--- src/App.js
+++ src/App.js
@@ -1,6 +1,16 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Nav from "components/Nav";
+import Teachers from "pages/ParentsPages/Teachers";
+import Request from "pages/ParentsPages/Request";
+import RequestDetail from "pages/ParentsPages/RequestDetail";
+import VisitSchedule from "pages/ParentsPages/ VisitSchedule";
+import VisitLog from "pages/ParentsPages/VisitLog";
+import ClassRecommendation from "pages/TeacherPages/ClassRecommendation";
+import ClassSupport from "pages/TeacherPages/ ClassSupport";
+import ClassSchedule from "pages/TeacherPages/ClassSchedule";
+import TeacherVisit from "pages/TeacherPages/TeacherVisit";
+import TeacherSchedule from "pages/TeacherPages/TeacherSchedule";
 import Home from "pages/Home/Home";
 import GlobalStyles from "styles/GlobalStyles";
 
@@ -11,6 +21,16 @@ function App() {
       <GlobalStyles />
       <Switch>
         <Route path="/" exact component={Home} />
+        <Route path="/teachers" component={Teachers} />
+        <Route path="/request" component={Request} />
+        <Route path="/request_detail" component={RequestDetail} />
+        <Route path="/visit_schedule" component={VisitSchedule} />
+        <Route path="/visit_log" component={VisitLog} />
+        <Route path="/class_recommendation" component={ClassRecommendation} />
+        <Route path="/class_support" component={ClassSupport} />
+        <Route path="/class_schedule" component={ClassSchedule} />
+        <Route path="/teacher_visit" component={TeacherVisit} />
+        <Route path="/teacher_schedule" component={TeacherSchedule} />
       </Switch>
     </Router>
   );
---
+++ src/pages/TeacherPages/ClassRecommendation.js
@@ -0,0 +1,8 @@
+import React from "react";
+import styled from "styled-components";
+
+function ClassRecommendation() {
+  return <h1>수업추천</h1>;
+}
+
+export default ClassRecommendation;
