23e5fff9323065b0828e71c42d632d91f3c8a940
Jiyeol Jake Lee <jake.lee4006@gmail.com>
2021-08-03 14:55:50 +0900

    [FEAT] #8 - api 추가 제작
    - getAllUsers
    - getAllMenus
    - getAllRoles

--- src/api/user/index.js
+++ src/api/user/index.js
@@ -29,3 +29,37 @@ export const createUser = ({ userId, password, role = 'parents', name, age, addr
 			console.error('Error adding document: ', error);
 		});
 };
+
+export const getAllUsers = () => {
+	return db
+		.collection('user')
+		.get()
+		.then((querySnapshot) => {
+			const result = [];
+			querySnapshot.forEach((doc) => {
+				// console.log({ ...doc.data(), id: doc.id });
+				result.push({ ...doc.data(), id: doc.id });
+			});
+			return result;
+		})
+		.catch((error) => {
+			console.error(error);
+		});
+};
+
+/**
+ * WIP(Work In Progress) - 아직 미완성, 제작 중..
+ */
+// export const findUserByIdAndPassword = (inputId, inputPassword) => {
+// 	const usersRef = db.collection('user');
+
+// 	usersRef
+// 		.where('id', '==', inputId)
+// 		.get()
+// 		.then((querySnapshot) => {
+// 			console.log({ ...doc.data(), id: doc.id });
+// 		})
+// 		.catch((error) => {
+// 			console.error(error);
+// 		});
+// };
