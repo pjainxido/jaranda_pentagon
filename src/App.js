5535069062327a0b1bcc8abb51201590f689593e
JSWww <tjddnjs9497@naver.com>
2021-08-05 13:08:18 +0900

    [FIX] import 수정

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from "react";
 import { BrowserRouter as Router, Switch } from "react-router-dom";
-import { Nav } from "components/Nav";
-import { Teachers, Parent } from "pages/Parent";
-import Admin from "pages/Admin";
-import SignUp from "pages/SignUp";
-import Login from "pages/Login";
-
 import { PublicRoute, PrivateRoute } from "components/Route";
+import Nav from "components/Nav";
+import Login from "pages/Login";
+import SignUp from "pages/SignUp";
+import Admin from "pages/Admin";
+import AdminRolePageView from "pages/Admin";
+import { Teachers, Parent } from "pages/Parent";
 
 function App() {
   return (
