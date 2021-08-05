38c78fa137c469cab09ac81eba4d88c5199b3b9c
salybu <66893123+salybu@users.noreply.github.com>
2021-08-05 23:10:51 +0900

    [FEAT] 회원가입 컴포넌트 분리 및 수정 (#66)

--- src/components/UserCreate/UserCreate.js
+++ src/components/UserCreate/UserCreate.js
@@ -1,12 +1,322 @@
-import React, { useEffect } from "react";
-import { useState } from "react";
+import React, { useState, useRef } from "react";
+import { createPortal } from "react-dom";
+import PropTypes from "prop-types";
+
 import { createUser, checkUserByUserId } from "api/user/index";
+import ToastPortal from "components/common/ToastPortal";
+import AddressApi from "components/AddressApi/AddressApi";
+import { CreditCardPopup } from "components/CreditCardPopup";
 
 import styled from "styled-components";
+import theme from "styles/theme";
 import loginTheme from "styles/loginTheme";
+import TOAST from "constants/toast";
+
+// eslint-disable-next-line react/prop-types
+const UserCreate = ({ props, setIsShown, isAdmin = false }) => {
+  const toastRef = useRef();
+  const [inputs, setInputs] = useState({
+    id: "",
+    idConfirm: false,
+    pw: "",
+    pwConfirm: "",
+    name: "",
+    age: "",
+    role: "parent",
+    address: "",
+    addressDetail: "",
+    isDaumPost: false,
+    cardNumber: "",
+    effectiveDate: "",
+    cvc: "",
+    pwValidCheck: false,
+  });
+
+  const [isCreditClick, setIsCreditClick] = useState(false);
+
+  const pwAlert = useRef(null);
+  const pwConfirmAlert = useRef(null);
+  const pwValidCheck = useRef(false);
+  const pwConfirmCheck = useRef(false);
+
+  const onChange = (e) => {
+    let { value, name } = e.target;
+    pwValidation(e);
+
+    setInputs({
+      ...inputs,
+      [name]: value,
+    });
+  };
+
+  const addToast = (mode, message) => {
+    toastRef.current.addMessage({ mode, message });
+  };
+
+  const pwValidation = (e) => {
+    let { value, name } = e.target;
+
+    if (name == "pw") {
+      const regExp = /^(?=.*\d)(?=.*[a-zA-Z])[0-9a-zA-Z]{8,10}$/;
+      let pwValid = regExp.test(value);
+
+      if (pwValid) {
+        pwAlert.current.innerHTML = "비밀번호 양식에 맞게 입력하였습니다";
+        pwAlert.current.style.color = theme.colors.green;
+        pwValidCheck.current = true;
+      } else {
+        pwAlert.current.innerHTML = "영문 및 숫자 포함 8 ~ 10자 작성해주세요";
+        pwAlert.current.style.color = theme.colors.red;
+        pwValidCheck.current = false;
+      }
+    }
+
+    if (name == "pwConfirm") {
+      var pwConfirmValid = inputs.pw == value;
+
+      if (pwConfirmValid) {
+        pwConfirmAlert.current.innerHTML = "비밀번호가 일치합니다";
+        pwConfirmAlert.current.style.color = theme.colors.green;
+        pwConfirmCheck.current = true;
+      } else {
+        pwConfirmAlert.current.innerHTML = "비밀번호가 일치하지 않습니다";
+        pwConfirmAlert.current.style.color = theme.colors.red;
+        pwConfirmCheck.current = false;
+      }
+    }
+  };
+
+  const checkSameId = async () => {
+    if (inputs.id.trim() !== "") {
+      var checked = await checkUserByUserId(inputs.id);
+      if (checked) {
+        addToast(TOAST.MODE.INFO, "작성한 아이디로 가입할 수 있습니다");
+        setInputs({
+          ...inputs,
+          idConfirm: true,
+        });
+      } else {
+        addToast(TOAST.MODE.ERROR, "중복된 아이디입니다");
+      }
+    } else {
+      addToast(TOAST.MODE.ERROR, "아이디를 입력하세요");
+    }
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    let valid = validation();
+
+    let reg = /[ \{\}\[\]\/?.,;:|\)*~`!^\-_+┼<>@\#$%&\ '\"\\(\=]/gi;
+    let cardNumber = inputs.cardNumber;
+    let effectiveDate = inputs.effectiveDate;
+    if (reg.test(inputs.cardNumber)) {
+      cardNumber = inputs.cardNumber.replace(reg, "");
+    }
+    if (reg.test(inputs.effectiveDate)) {
+      effectiveDate = inputs.effectiveDate.replace(reg, "");
+    }
+
+    if (valid) {
+      let newUser = {
+        userId: inputs.id,
+        role: inputs.role,
+        password: inputs.pw,
+        name: inputs.name,
+        age: Number(inputs.age),
+        address: inputs.address + inputs.addressDetail,
+        creditCard: {
+          cardNumber: cardNumber,
+          expirtyDate: effectiveDate,
+          cvc: inputs.cvc,
+        },
+      };
+      console.log(newUser);
+      await createUser(newUser)
+        .then(() => {
+          if (isAdmin) {
+            setIsShown(false);
+          } else {
+            addToast(TOAST.MODE.SUCCESS, "성공적으로 회원 가입하셨습니다!! 🎊");
+            props.history.replace("/");
+          }
+        })
+        .catch((error) => {
+          if (!isAdmin) {
+            console.error("creating user Error: ", error);
+            addToast(TOAST.MODE.ERROR, "회원 가입 시 에러가 발생하였습니다 😥");
+            window.location.reload();
+          }
+        });
+    }
+  };
+
+  const validation = () => {
+    if (inputs.id == "") {
+      addToast(TOAST.MODE.ERROR, "Id를 입력하세요");
+      return false;
+    }
+    if (inputs.idConfirm === false) {
+      addToast(TOAST.MODE.ERROR, "아이디 중복확인을 해주세요");
+      return false;
+    }
+    if (inputs.pw == "" || inputs.pw == undefined) {
+      addToast(TOAST.MODE.ERROR, "비밀번호를 입력하세요");
+      return false;
+    }
+    if (!pwValidCheck.current) {
+      addToast(TOAST.MODE.ERROR, "비밀번호를 양식에 맞게 입력하세요");
+      return false;
+    }
+    if (!pwConfirmCheck.current) {
+      addToast(TOAST.MODE.ERROR, "비밀번호를 확인하세요");
+      return false;
+    }
+    if (inputs.address == "" || inputs.addressDetail == "") {
+      addToast(TOAST.MODE.ERROR, "주소 및 상세주소를 입력하세요");
+      return false;
+    }
+    if (
+      inputs.cardNumber == "" ||
+      inputs.effectiveDate == "" ||
+      inputs.cvc == ""
+    ) {
+      addToast(TOAST.MODE.ERROR, "신용카드 정보를 입력하세요");
+      return false;
+    }
+    return true;
+  };
+
+  const handlePostModal = () => {
+    setInputs({ ...inputs, isDaumPost: true });
+  };
+
+  return (
+    <WiderContent>
+      <NarrowContent>
+        {!isAdmin && (
+          <Title>
+            10초 만에 가입하고<br></br>선생님 정보를 받아보세요
+          </Title>
+        )}
+        <ButtonWrap>
+          <StyledInput
+            placeholder="아이디"
+            name="id"
+            value={inputs.id}
+            onChange={onChange}
+          ></StyledInput>
+          <StyledButton onClick={checkSameId}>아이디 중복확인</StyledButton>
+        </ButtonWrap>
+        <StyledInput
+          placeholder="비밀번호"
+          name="pw"
+          type="password"
+          value={inputs.pw}
+          onChange={onChange}
+          style={{ marginBottom: 5 }}
+        ></StyledInput>
+        {/* {inputs.pwValidCheck ? (
+          <Confirm>영문 및 숫자 포함 8 ~ 10자 작성해주세요</Confirm>
+        ) : (
+          <Alert>영문 및 숫자 포함 8 ~ 10자 작성해주세요</Alert>
+        )} */}
+        <Alert valid={false} ref={pwAlert}>
+          영문 및 숫자 포함 8 ~ 10자 작성해주세요
+        </Alert>
+        <StyledInput
+          placeholder="비밀번호 확인"
+          name="pwConfirm"
+          type="password"
+          value={inputs.pwConfirm}
+          style={{ marginBottom: 5 }}
+          onChange={onChange}
+        ></StyledInput>
+        <Alert valid={false} ref={pwConfirmAlert}>
+          비밀번호가 일치하지 않습니다
+        </Alert>
+        <StyledInput
+          placeholder="이름"
+          name="name"
+          value={inputs.name}
+          onChange={onChange}
+        ></StyledInput>
+        <StyledInput
+          placeholder="나이"
+          name="age"
+          value={inputs.age}
+          onChange={onChange}
+        ></StyledInput>
+        {isAdmin && (
+          <RoleSelect name="role" onChange={onChange} value={inputs.role}>
+            <option value="parent">부모님</option>
+            <option value="teacher">선생님</option>
+          </RoleSelect>
+        )}
+        <ButtonWrap>
+          <StyledInput
+            readOnly
+            placeholder="주소"
+            value={inputs.address}
+          ></StyledInput>
+          <StyledButton onClick={handlePostModal}>주소 검색</StyledButton>
+          {inputs.isDaumPost && (
+            <AddressApi inputs={inputs} setInputs={setInputs} />
+          )}
+        </ButtonWrap>
+        <StyledInput
+          placeholder="상세 주소"
+          name="addressDetail"
+          value={inputs.addressDetail}
+          onChange={onChange}
+        ></StyledInput>
+        <ButtonWrap>
+          <StyledInput
+            readOnly
+            placeholder="카드 번호"
+            name="cardNumber"
+            value={inputs.cardNumber}
+          ></StyledInput>
+          <StyledButton onClick={() => setIsCreditClick(true)}>
+            신용카드 입력
+          </StyledButton>
+        </ButtonWrap>
+        <ButtonWrap>
+          <StyledInput
+            readOnly
+            placeholder="유효 기간"
+            name="effectiveDate"
+            value={inputs.effectiveDate}
+            style={{ marginRight: 10 }}
+          ></StyledInput>
+          <StyledInput
+            readOnly
+            placeholder="CVC 번호"
+            name="cvc"
+            value={inputs.cvc}
+          ></StyledInput>
+        </ButtonWrap>
+        <form onSubmit={handleSubmit}>
+          <StyledButton type="submit" style={{ width: "100%" }}>
+            {isAdmin ? "계정생성" : "회원가입"}
+          </StyledButton>
+        </form>
+      </NarrowContent>
+      {isCreditClick &&
+        createPortal(
+          <CreditCardPopup
+            onClose={() => setIsCreditClick(false)}
+            saveCardInfo={setInputs}
+          />,
+          document.getElementById("user-modal-root")
+        )}
+      <div id="user-modal-root"></div>
+      <ToastPortal ref={toastRef} position={TOAST.POSITION.TOP_CENTER} />
+    </WiderContent>
+  );
+};
 
 const {
-  Container,
   WiderContent,
   NarrowContent,
   ButtonWrap,
@@ -30,120 +340,21 @@ const RoleSelect = styled.select`
   padding: 0 15px;
 `;
 
-// eslint-disable-next-line react/prop-types
-function UserCreate({ setIsShown }) {
-  const [inputs, setInputs] = useState({
-    id: "",
-    idConfirm: false,
-    pw: "",
-    name: "",
-    age: "",
-    role: "parent",
-    address: "",
-  });
+const Alert = styled.div`
+  font-size: 14px;
+  color: ${({ theme }) => theme.colors.red};
+  text-align: left;
+  padding-bottom: 15px;
+`;
 
-  const onChange = (e) => {
-    let { value, name } = e.target;
-    setInputs({
-      ...inputs,
-      [name]: value,
-    });
-  };
+const Confirm = styled(Alert)`
+  color: ${({ theme }) => theme.colors.green};
+`;
 
-  const checkSameId = async () => {
-    let checked = await checkUserByUserId(inputs.id);
-    if (checked) {
-      alert("아이디로 가입할 수 있습니다");
-      setInputs({
-        ...inputs,
-        idConfirm: true,
-      });
-    } else {
-      alert("중복된 아이디입니다");
-    }
-  };
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    let valid = validation();
-
-    if (valid) {
-      let newUser = {
-        userId: inputs.id,
-        role: inputs.role,
-        password: inputs.pw,
-        name: inputs.name,
-        age: Number(inputs.age),
-        address: inputs.address,
-        creditCard: {},
-      };
-      await createUser(newUser);
-      setIsShown(false);
-    }
-  };
-
-  const validation = () => {
-    if (inputs.id == "") {
-      alert("Id를 입력하세요");
-      return false;
-    }
-    if (inputs.idConfirm === false) {
-      alert("아이디 중복확인을 해주세요");
-      return false;
-    }
-    if (inputs.pw == "" || inputs.pw == undefined) {
-      alert("비밀번호를 입력하세요");
-      return false;
-    }
-    return true;
-  };
-
-  return (
-    // <Container>
-    <WiderContent>
-      <NarrowContent>
-        <Title>계정 임의생성</Title>
-        <ButtonWrap>
-          <StyledInput
-            placeholder="아이디"
-            name="id"
-            value={inputs.id}
-            onChange={onChange}
-          ></StyledInput>
-          <StyledButton onClick={checkSameId}>아이디 중복확인</StyledButton>
-        </ButtonWrap>
-        <StyledInput
-          placeholder="비밀번호"
-          name="pw"
-          value={inputs.pw}
-          onChange={onChange}
-        ></StyledInput>
-        <StyledInput
-          placeholder="이름"
-          name="name"
-          value={inputs.name}
-          onChange={onChange}
-        ></StyledInput>
-        <StyledInput
-          placeholder="나이"
-          name="age"
-          value={inputs.age}
-          onChange={onChange}
-        ></StyledInput>
-        <RoleSelect name="role" onChange={onChange} value={inputs.role}>
-          <option value="parent">부모님</option>
-          <option value="teacher">선생님</option>
-        </RoleSelect>
-        <StyledInput placeholder="주소"></StyledInput>
-        <form onSubmit={handleSubmit}>
-          <StyledButton type="submit" style={{ width: "100%" }}>
-            계정생성
-          </StyledButton>
-        </form>
-      </NarrowContent>
-    </WiderContent>
-    // </Container>
-  );
-}
+UserCreate.propTypes = {
+  props: PropTypes.any,
+  history: PropTypes.any,
+  isAdmin: PropTypes.bool,
+};
 
 export default UserCreate;
