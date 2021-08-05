1c1c87a7de63ba71209b3a6422ea2462a9ae0037
JSWww <tjddnjs9497@naver.com>
2021-08-05 22:55:10 +0900

    [FEAT] 사용자별 메뉴 클릭 시 각 메뉴 페이지로 이동 구현

--- src/pages/Parent/index.js
+++ src/pages/Parent/index.js
@@ -1,6 +1,1 @@
-export { default as Request } from "./Request";
-export { default as RequestDetail } from "./RequestDetail";
-export { default as Teachers } from "./Teachers";
-export { default as VisitSchedule } from "./VisitSchedule";
-export { default as VisitLog } from "./VisitLog";
 export { default as Parent } from "./Parent";
--- src/pages/Teacher/index.js
+++ src/pages/Teacher/index.js
@@ -1,5 +1,1 @@
-export { default as ClassRecommendation } from "./ClassRecommendation";
-export { default as ClassSchedule } from "./ClassSchedule";
-export { default as ClassSupport } from "./ClassSupport";
-export { default as TeacherSchedule } from "./TeacherSchedule";
-export { default as TeacherVisit } from "./TeacherVisit";
+export { default as Teacher } from "./Teacher";
