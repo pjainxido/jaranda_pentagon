0f73c62014417c31b3e69e9d66e454f33e0aede4
Park <pjane123@naver.com>
2021-08-05 10:58:41 +0900

    [FEAT]constants 상수 데이터 추가
    - modalRoot id 값 추가
    - toastPortal내에 사용되는 상수 추가

---
+++ src/constants/toast.js
@@ -0,0 +1,18 @@
+const TOAST = {
+  MODE: {
+    SUCCESS: "success",
+    INFO: "info",
+    WARNING: "warning",
+    ERROR: "error",
+  },
+  POSITION: {
+    TOP_LEFT: "top-left",
+    TOP_CENTER: "top-center",
+    TOP_RIGHT: "top-right",
+    BOT_LEFT: "bottom-left",
+    BOT_CENTER: "bottom-center",
+    BOT_RIGHT: "bottom-right",
+  },
+};
+
+export default TOAST;
