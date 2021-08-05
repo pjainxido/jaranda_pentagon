d748d673a8332bd9a999c9155ffc2f817e1dc77a
JSWww <tjddnjs9497@naver.com>
2021-08-05 22:56:49 +0900

    [FEAT] 사용자별로 메뉴 렌더링 구현 (#65)
    * [CHORE] menu 컴포넌트 pages/Menu로 모아둠
    
    * [FEAT] menu 컴포넌트와 route를 맵핑하는 util 추가
    
    * [FEAT] 사용자별 메뉴 클릭 시 각 메뉴 페이지로 이동 구현

---
+++ src/utils/menuRouteMapping.js
@@ -0,0 +1,27 @@
+import {
+  ClassSchedule,
+  ClassRecommendation,
+  Teachers,
+  Request,
+  RequestDetail,
+  TeacherSchedule,
+  VisitSchedule,
+  VisitLog,
+  ClassSupport,
+  TeacherVisit,
+} from "pages/Menu";
+
+const menuRouteMapping = {
+  class_schedule: ClassSchedule,
+  class_recommendation: ClassRecommendation,
+  teachers: Teachers,
+  request: Request,
+  request_detail: RequestDetail,
+  teacher_schedule: TeacherSchedule,
+  visit_schedule: VisitSchedule,
+  visit_log: VisitLog,
+  class_support: ClassSupport,
+  teacher_visit: TeacherVisit,
+};
+
+export default menuRouteMapping;
