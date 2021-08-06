e57f8e478aea71d6a37d859fc97314bb8e2aeb86
JSWww <tjddnjs9497@naver.com>
2021-08-06 15:01:47 +0900

    [REFACTOR] route path 상수화

---
+++ src/constants/routePath.js
@@ -0,0 +1,20 @@
+const ROUTE_PATH = {
+  MAIN: '/',
+  SIGN_UP: '/signup',
+  ADMIN: '/admin',
+  PARENT: '/parent',
+  TEACHER: '/teacher',
+  MANAGE_ROLE: '/admin/manage_role',
+  CLASS_SCHEDULE: '/class_schedule',
+  CLASS_RECOMMENDATION: '/class_recommendation',
+  TEACHERS: '/teachers',
+  REQUEST: '/request',
+  REQUEST_DETAIL: '/request_detail',
+  TEACHER_SCHEDULE: '/teacher_schedule',
+  VISIT_SCHEDULE: '/visit_schedule',
+  VISIT_LOG: '/visit_log',
+  CLASS_SUPPORT: '/class_support',
+  TEACHER_VISIT: '/teacher_visit',
+};
+
+export default ROUTE_PATH;
--- src/utils/menuRouteMapping.js
+++ src/utils/menuRouteMapping.js
@@ -10,18 +10,19 @@ import {
   ClassSupport,
   TeacherVisit,
 } from 'pages/Menu';
+import ROUTE_PATH from 'constants/routePath';
 
 const menuRouteMapping = {
-  class_schedule: ClassSchedule,
-  class_recommendation: ClassRecommendation,
-  teachers: Teachers,
-  request: Request,
-  request_detail: RequestDetail,
-  teacher_schedule: TeacherSchedule,
-  visit_schedule: VisitSchedule,
-  visit_log: VisitLog,
-  class_support: ClassSupport,
-  teacher_visit: TeacherVisit,
+  [ROUTE_PATH.CLASS_SCHEDULE]: ClassSchedule,
+  [ROUTE_PATH.CLASS_RECOMMENDATION]: ClassRecommendation,
+  [ROUTE_PATH.TEACHERS]: Teachers,
+  [ROUTE_PATH.REQUEST]: Request,
+  [ROUTE_PATH.REQUEST_DETAIL]: RequestDetail,
+  [ROUTE_PATH.TEACHER_SCHEDULE]: TeacherSchedule,
+  [ROUTE_PATH.VISIT_SCHEDULE]: VisitSchedule,
+  [ROUTE_PATH.VISIT_LOG]: VisitLog,
+  [ROUTE_PATH.CLASS_SUPPORT]: ClassSupport,
+  [ROUTE_PATH.TEACHER_VISIT]: TeacherVisit,
 };
 
 export default menuRouteMapping;
